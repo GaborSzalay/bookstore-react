f5c76ae04eb1cdf21dceb1b9082284bfe31aad2c
Gabor Szalay <yaastreet@gmail.com>
2017-09-11 17:24:55 +0200

    Adding search component

---
+++ src/Search.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchValue: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleChange(event) {
+        this.setState({searchValue: event.target.value});
+    }    
+    
+    render() {
+        return (
+            <div className="Search">
+                <input type="text" name="search" value={this.state.searchValue} onChange={this.handleChange} />
+            </div>
+        );
+    }
+}
+
+export default Search;
