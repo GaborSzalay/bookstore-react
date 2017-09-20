79f2cd9a9043e6981ad0eb81fe23fb92b0e6e22b
Gabor Szalay <yaastreet@gmail.com>
2017-09-20 16:20:34 +0200

    reusing data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import logo from './logo.svg';
 import './App.css';
 import Search from './Search';
+
+const mapStateToProps = (state) => {
+  return {searchValue: state.searchValue};
+};
 
 class App extends Component {
   render() {
@@ -10,6 +15,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <span>{this.props.searchValue}</span>
         </div>
         <Search />
       </div>
@@ -17,4 +23,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect( mapStateToProps )(App);
